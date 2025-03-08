3a8aca54a69a5191198f55c900ee04c1aa59ae8c
Shaun Croft <shaun@redleafsoft.com>
2025-03-08 09:18:58 -0500

    Fixed a bunch of errors

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:seegle/styles.dart';
 import 'package:seegle/widgets/custom_dialog.dart';
+import 'add_flock_button.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   final Function(int) onTap;
@@ -24,70 +25,76 @@ class _CustomBottomNavigationBar extends State<CustomBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.home_outlined),
-            iconSize: 30,
-            onPressed: () {
-              setIndex(0);
-            },
-            color: currentIndex == 0 ? AppColors.darkGrey : AppColors.lightGrey,
-          ),
-          CupertinoButton(
-            child: Container(
-              decoration: BoxDecoration(
-                color: AppColors.darkGrey,
-                // color: Colors.blue,
-                borderRadius:
-                    BorderRadius.circular(50), // 50% of the container's height
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 20),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.add_sharp,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      'New Squawk',
-                      style: TextStyle(
+    return Container(
+      color: Color(0xFFeeeeee),
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home_outlined),
+              iconSize: 30,
+              onPressed: () {
+                setIndex(0);
+              },
+              color:
+                  currentIndex == 0 ? AppColors.darkGrey : AppColors.mediumGrey,
+            ),
+            CupertinoButton(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.darkGrey,
+                  // color: Colors.blue,
+                  borderRadius: BorderRadius.circular(
+                      50), // 50% of the container's height
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 20),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.add_sharp,
                         color: Colors.white,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'New Flock',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            onPressed: () {
-              showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (BuildContext context) {
-                  // Dialog cannot be dismissed by tapping outside
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    // Dialog cannot be dismissed by tapping outside
 
-                  return const MyCustomDialog();
-                },
-              );
-            },
-          ),
-          IconButton(
-            iconSize: 30,
-            icon: const Icon(Icons.person_outline),
-            onPressed: () {
-              setIndex(1);
-              currentIndex = 1;
-            },
-            color: currentIndex == 1 ? AppColors.darkGrey : AppColors.lightGrey,
-          ),
-        ],
+                    return const MyCustomDialog();
+                  },
+                );
+              },
+            ),
+            const AddFlockButton(),
+            IconButton(
+              iconSize: 30,
+              icon: const Icon(Icons.person_outline),
+              onPressed: () {
+                setIndex(1);
+                currentIndex = 1;
+              },
+              color:
+                  currentIndex == 1 ? AppColors.darkGrey : AppColors.mediumGrey,
+            ),
+          ],
+        ),
       ),
     );
   }
